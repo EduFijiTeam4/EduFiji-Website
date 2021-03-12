52fe9604c0cd0d4b2eb88b9682d031380a38a082
EduFijiTeam4 <80551243+EduFijiTeam4@users.noreply.github.com>
2021-03-13 10:54:55 +1200

    Add files via upload

---
+++ EduFiji/AI chatbot/nltk.py
@@ -0,0 +1,34 @@
+import numpy as np
+import nltk
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words:
+            bag[idx] = 1
+
+a = "What subject do you need help with"
+print(a)
+a= tokenize(a)
+    return bag
+    print(a)
